6b3cdf5eb6147939179a0f4637b7369104a2a64b
Ronnie Perez <Ronaldaperez95@gmail.com>
2024-11-04 18:15:46 -0600

    Added 605 planting pots

--- 605PlantingPots.cpp
+++ 605PlantingPots.cpp
@@ -1,8 +1,30 @@
-#include <iostream>
+class Solution {
+public:
+    bool canPlaceFlowers(vector<int>& flowerbed, int n) {
+        // if n == 0 then there is no plants to be placed
+        if (n == 0) {
+            return true;
+        }
 
-int main() {
+        for (int i = 0; i < flowerbed.size(); i++) {
+            // flowerbed[i] == 0 then we might be able to place a flower here
+            // AND
+            // i == 0 || flowerbed[i-1], either i is 0 OR flowerbed[i-1] (previous value needs to be checked for 0) if its 1 then we cannot add the flower.
+            // AND
+            // i == flowerbed.size() -1 || flowerbed[i+1] == 0, this checks if i is the last position in the array or if its empty, if it's not the last value
+            // then it will check the right value for 0. 
+            // if all of these pass then a plant is plantable.
+            if (flowerbed[i] == 0 && (i == 0 || flowerbed[i-1] == 0) && (i == flowerbed.size() - 1 || flowerbed[i+1] == 0)) {
+                // if the flower bed fits all the parameters, then that i value can now be placed as a 1
+                flowerbed[i] = 1;
 
-    
-
-    return 0;
-}
+                // then minus 1 value of n because we just placed it.
+                n--;
+                if (n == 0) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+};
---
+++ ADTsImplementation/test.cpp
@@ -0,0 +1,18 @@
+// Implement these functions -> insertAfter, insertEnd, insertFront, Delete, BubbleSort
+
+#include <iostream>
+
+class Node {
+    public:
+        int value;
+        Node* next;
+
+};
+int main() {
+
+    // Creating the Head Node and givig it data
+    Node* head = new Node();
+    head->value = 
+
+    return 0;
+}
